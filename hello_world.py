c7f1dd6914c413ec43994bdd84856ed58de87123
ishtyaq-mv <rao.ishtiaq@gmail.com>
2022-12-10 14:27:23 +0400

    Added a message from user 1

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    msg = "user 1: "
+    return msg + "Hello, World!"
+
 
 if __name__ == "__main__":
     app.run()
